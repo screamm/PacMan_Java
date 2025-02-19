4a87ac73569915070777f7c7b89518ccef66492e
Screamm <davidrydgren@gmail.com>
2025-02-19 23:00:37 +0100

    Initial push

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+    int rowCount = 42;
+    int columnCount = 80;
+    int tileSize = 32;
+    int boardWidth = columnCount * tileSize;
+    int boardHeight = rowCount * tileSize;
+
+    JFrame frame = new JFrame("Pac");
+    frame.setVisible(true);
+    frame.setSize(boardWidth, boardHeight);
+    frame.setLocationRelativeTo(null);
+    frame.setResizable(false);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+ 
+        
+    }
+}
