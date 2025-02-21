456af68178dd5a2494b9b504a37eda398e63754a
Screamm <davidrydgren@gmail.com>
2025-02-22 00:14:10 +0100

    Packman logik

--- src/PacMan.java
+++ src/PacMan.java
@@ -48,6 +48,59 @@ public class PacMan extends JPanel {
     private Image pacmanRightImage;
 
 
+
+
+
+ //X = wall, O = skip, P = pac man, ' ' = food
+    //Ghosts: b = blue, o = orange, p = pink, r = red
+    private String[] tileMap = {
+        "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "XXXXXXXXXXXXX                             XXXXXX    XXXXXXXXXXXX",
+        "X XX XXX X XXX XX XXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "X                 XXXXXXXXXXXXXXXX    XXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "X XX X XXXXX X XX XXXXXXXXXXXXXXXX     XXXXXXXXXXXXXXXXXXXXXXXXX",
+        "X    X       X       XXXXXXXXXXXXX     XXXXXXXXXXXXXXXXXXXXXXXXX",
+        "XXXX XXXX XXXX XXXX   XXXXXXXXXXX      XXXXXXXXXXXXXXXXXXXXXXXXX",
+        "OOOX X       X XOOO   XXXXXXXXXXX      XXXXXXXXXXXXXXXXXXXXXXXXX",
+        "XXXX X XXrXX X XXXX   XXXXXXXXXXX      XXXXXXXXXXXXXXXXXXXXXXXXX",
+        "O       bpo                                                    O",
+        "XXXX X XXXXX X                                              XXXX",
+        "OOOX X       X                                              XOOO",
+        "XXXX X XXXXX X                                              XXXX",
+        "X        X                                                     X",
+        "X XX XXX X XXX XX                                              X",
+        "X  X     P     X                                               X",
+        "XX X X XXXXX X X                                              XX",
+        "X    X   X   X                                                 X",
+        "X XXXXXX X XXXXXX                                              X",
+        "X                                                              X",
+        "XXXXXXXXXXXXXXXXXXX                  XXXXXXXX                  X", 
+        "XXXXXXXXXXXXXXXXXXX                   xxxxxxx                  X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX               xxxxxxxxxxxxxxxxxxx          X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX                   xxxxxxxxxxxxxx           X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX          xxxxxxxx                          X",
+        "XXXXXXXXXXXXXXXXXXX                 x                          X",
+        "XXXXXXXXXXXXXXXXXXX                 x          xxxxxxxxxxx     X",
+        "XXXXXXXXXXXXXXXXXXX                 x                          X", 
+        "XXXXXXXXXXXXXXXXXXX                 x                          X",
+        "XXXXXXXXXXXXXXXXXXX                 x                          X",
+        "XXXXXXXXXXXXXXXXXXX               xxxxxxxxxxxxxxxxxxxx         X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXX                                            X",
+        "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+
+    };
+
+
+
 HashSet<Block> walls;
 HashSet<Block> ghosts;
 HashSet<Block> foods;
@@ -72,10 +125,50 @@ Block pacman;
         pacmanDownImage = new ImageIcon(getClass().getResource("/images/pacmanDown.png")).getImage();
         pacmanLeftImage = new ImageIcon(getClass().getResource("/images/pacmanLeft.png")).getImage();
         pacmanRightImage = new ImageIcon(getClass().getResource("/images/pacmanRight.png")).getImage();
-
-
         
     }
-    
-    
+
+    public void loadMap() {
+        walls = new HashSet<Block>();
+        foods = new HashSet<Block>();
+        ghosts = new HashSet<Block>();
+        
+        for (int row = 0; row < rowCount; row++) {
+            for (int c = 0; c < columnCount; c++) {
+              String row = tileMap[r];
+              char tile = row.charAt(c);
+            
+                int x = c*tileSize;
+                int y = r*tileSize;
+
+                if (tileMapChar == "X") { // block wall
+                    Block wall = new Block(wallImage, x, y, tileSize, tileSize);
+                    walls.add(wall);
+                }
+                else if (tileMapChar == 'b') //blue ghost
+                Block ghost = new Block(blueGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+                }
+                else if (tileMapChar == 'o') //orange ghost
+                Block ghost = new Block(orangeGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+                }
+                else if (tileMapChar == 'p') //pink ghost
+                Block ghost = new Block(pinkGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+                }
+                else if (tileMapChar == 'r') //red ghost
+                Block ghost = new Block(redGhostImage, x, y, tileSize, tileSize);
+                ghosts.add(ghost);
+                }
+                else if (tileMapChar == 'P') //pacman
+                pacman = new Block(pacmanRightImage, x, y, tileSize, tileSize);
+                }
+         
+                else if (tileMapChar == 'P') { //pacman
+                    pacman = new Block(pacmanRightImage, x, y, tileSize, tileSize);
+                }   
+
+
+            
 }
